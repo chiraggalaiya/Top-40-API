19ade4d9633b0458ebc826ca8ba41dcace86aef0
Chirag Galaiya <admin@chilichingching.16mb.com>
2016-05-18 20:52:25 +0100

    Initial Release

---
+++ README.md
@@ -0,0 +1,188 @@
+# Top 40 API
+
+Simple JavaScript API for BBC Top 40 Charts (11 different charts!)
+
+Basic useful feature list:
+
+ * Receive info from more than 10 different charts!
+ * Get track info easily
+ * Returns Array if you request full list or
+ * Returns String if you request info for a single track
+
+## Setup
+
+Download the [top40.js](http://raw.githubusercontent.com/chiraggalaiya/top40api/master/top40.js) file and add the following code to your HTML file's HEAD tag:
+
+```html
+<script src="top40.js"></script>
+```
+
+## Initializing API
+
+Add the following code to your javascript:
+```javascript
+var top40 = new top40([type]);
+```
+The *type* parameter is OPTIONAL and is for the *type* of chart. The default is `singles`. Other options are:
+
+ * `singles`
+ * `albums`
+ * `dancesingles`
+ * `dancealbums`
+ * `indiesingles`
+ * `indiealbums`
+ * `rnbalbums`
+ * `rnbsingles`
+ * `rocksingles`
+ * `rockalbums`
+ * `compilationalbums`
+
+## Methods
+All methods are listed below:
+ * [`top40.getTitles()`](#getTitles)
+ * [`top40.getTitle(position)`](#getTitle)
+ * [`top40.getArtists()`](#getArtists)
+ * [`top40.getArtist(position)`](#getArtist)
+ * [`top40.getWeeksInChart()`](#getWeeksInChart)
+ * [`top40.getWeekInChart(position)`](#getWeekInChart)
+ * [`top40.getStates()`](#getStates)
+ * [`top40.getState(position)`](#getState)
+ * [`top40.getAlbumCovers()`](#getAlbumCovers)
+ * [`top40.getAlbumCover(position)`](#getAlbumCover)
+ * [`top40.getMovements()`](#getMovements)
+ * [`top40.getMovement(position)`](#getMovement)
+
+## getTitles
+This function will return an *array* which will contain all 40 titles (array[0] = 1st title, array[39] = 40th title)
+
+Example:
+```javascript
+var top40 = new top40(); // Initialize API
+var titles = top40.getTitles(); // Store Array in Variable
+console.log(titles[0]); // Logs 1st  Track's Title
+console.log(titles[39]); // Logs 40th  Track's Title
+```
+## getTitle
+This function will return a *string* which will contain the given track's title
+```javascript
+var title = new top40().getTitle(position); // 'position' is an Integer (1 to 40 inclusive)
+```
+
+Example:
+```javascript
+var top40 = new top40(); // Initialize API
+var title = top40.getTitle(19); // Store String in Variable
+console.log(title); // Logs 20th Track's Title
+```
+## getArtists
+This function will return an *array* which will contain all 40 artists (array[0] = 1st artist, array[39] = 40th artist)
+
+Example:
+```javascript
+var top40 = new top40(); // Initialize API
+var artists = top40.getArtists(); // Store Array in Variable
+console.log(artists[0]); // Logs 1st  Track's Artist
+console.log(artists[39]); // Logs 40th  Track's Artist
+```
+## getArtist
+This function will return a *string* which will contain the given track's artist
+```javascript
+var artist = new top40().getArtist(position); // 'position' is an Integer (1 to 40 inclusive)
+```
+
+Example:
+```javascript
+var top40 = new top40(); // Initialize API
+var artist = top40.getArtist(19); // Store String in Variable
+console.log(artist); // Logs 20th Track's Artist
+```
+## getWeeksInChart
+This function will return an *array* which will contain all 40 track's number of weeks in the chart (array[0] = 1st track's number of weeks in chart, array[39] = 40th track's number of weeks in chart)
+
+Example:
+```javascript
+var top40 = new top40(); // Initialize API
+var weeksInChart = top40.getWeeksInChart(); // Store Array in Variable
+console.log(weeksInChart[0]); // Logs 1st Track's Number Of Weeks In Chart
+console.log(weeksInChart[39]); // Logs 40th Track's Number Of Weeks In Chart
+```
+## getWeekInChart
+This function will return a *string* which will contain the given track's number of weeks in chart
+```javascript
+var weekInChart = new top40().getWeekInChart(position); // 'position' is an Integer (1 to 40 inclusive)
+```
+
+Example:
+```javascript
+var top40 = new top40(); // Initialize API
+var weekInChart = top40.getWeekInChart(19); // Store String in Variable
+console.log(weekInChart); // Logs 20th Track's Number Of Weeks In Chart
+```
+## getStates
+This function will return an *array* which will contain all 40 track states - Either 'NON-MOVER', 'NEW', 'UP [number]' or 'DOWN [number]' - (array[0] = 1st track's state, array[39] = 40th track's state)
+
+Example:
+```javascript
+var top40 = new top40(); // Initialize API
+var states = top40.getStates(); // Store Array in Variable
+console.log(states[0]); // Logs 1st Track's State
+console.log(states[39]); // Logs 40th Track's State
+```
+## getState
+This function will return a *string* which will contain the given track's state - Either 'NON-MOVER', 'NEW', 'UP [number]' or 'DOWN [number]'
+```javascript
+var state = new top40().getState(position); // 'position' is an Integer (1 to 40 inclusive)
+```
+
+Example:
+```javascript
+var top40 = new top40(); // Initialize API
+var state = top40.getState(19); // Store String in Variable
+console.log(state); // Logs 20th Track's State
+```
+## getAlbumCovers
+This function will return an *array* which will contain all 40 track's album covers - Link To JPEG Image - (array[0] = 1st track's album cover, array[39] = 40th track's album cover) - NOT ALL SIZES ARE THE SAME
+
+Example:
+```javascript
+var top40 = new top40(); // Initialize API
+var albumCovers = top40.getAlbumCovers(); // Store Array in Variable
+console.log(albumCovers[0]); // Logs 1st Track's Album Cover
+console.log(albumCovers[39]); // Logs 40th Track's Album Cover
+```
+## getAlbumCover
+This function will return a *string* which will contain the given track's album cover - Link To JPEG Image
+```javascript
+var albumCover = new top40().getAlbumCover(position); // 'position' is an Integer (1 to 40 inclusive)
+```
+
+Example:
+```javascript
+var top40 = new top40(); // Initialize API
+var albumCover = top40.getAlbumCover(19); // Store String in Variable
+console.log(albumCover); // Logs 20th Track's Album Cover
+```
+## getMovements
+This function will return an *array* which will contain all 40 track's movement - to indicate weather track has moved up, down, not moved or is new - (array[0] = 1st track's movement, array[39] = 40th track's movement) - YOU CAN CHANGE RESULTS FROM `top40_config` VARIABLE FOUND AT THE TOP OF `top40.js`
+
+Example:
+```javascript
+var top40 = new top40(); // Initialize API
+var movement = top40.getMovements(); // Store Array in Variable
+console.log(movement[0]); // Logs 1st Track's Movement
+console.log(movement[39]); // Logs 40th Track's Movement
+```
+## getMovement
+This function will return a *string* which will contain the given track's album cover - to indicate weather track has moved up, down, not moved or is new - YOU CAN CHANGE RESULTS FROM `top40_config` VARIABLE FOUND AT THE TOP OF `top40.js`
+```javascript
+var movement = new top40().getMovement(position); // 'position' is an Integer (1 to 40 inclusive)
+```
+
+Example:
+```javascript
+var top40 = new top40(); // Initialize API
+var movement = top40.getMovements(19); // Store String in Variable
+console.log(movement); // Logs 20th Track's Movement
+```
+## Contact Me
+You can all contact me at `chiraggalaiya@outlook.com`. Please contact me if you find bugs or glitches. Thank You.
---
+++ top40.js
@@ -0,0 +1,237 @@
+/*
+
+Top 40 API Developed And Built By Chirag Galaiya From http://chiragalaiya.uk/
+
+Top 40 Lists From BBC Radio 1
+
+Documentation : http://github.com/chiraggalaiya/top40api/blob/master/README.md
+
+*/
+
+
+
+// DO NOT DELETE
+var top40_config = [
+    'true', // Console Log Errors - 'true' to Console Log  or  'false' to Not Console Log
+    'upper', // State Result - Upper or Lower Case ('upper' for UPPER CASE  or  'lower' for lower case)
+    'new', // Movement Result For Newly Added Song
+    'up', // Movement Result For Song Which Has Moved Up
+    'down', // Movement Result For Song Which Has Moved Down
+    'non-mover' // Movement Result For Song Which Has Not Moved
+];
+
+
+
+function top40(type) {
+    if (typeof type === 'undefined') { type = 'singles'; }
+    if (type=='singles'||type=='albums'||type=='dancesingles'||type=='dancealbums'||type=='indiesingles'||type=='indiealbums'||type=='rnbalbums'||type=='rnbsingles'||type=='rocksingles'||type=='rockalbums'||type=='compilationalbums') {
+        this.getTitles = getTitles,
+        this.getTitle = getTitle,
+        this.getArtists = getArtists,
+        this.getArtist = getArtist,
+        this.getWeeksInChart = getWeeksInChart,
+        this.getWeekInChart = getWeekInChart,
+        this.getStates = getStates,
+        this.getState = getState,
+        this.getAlbumCovers = getAlbumCovers,
+        this.getAlbumCover = getAlbumCover,
+        this.getMovements = getMovements,
+        this.getMovement = getMovement;
+        
+        if (typeof top40_config == 'undefined') {
+            top40_config = ['true', 'upper', 'new', 'up', 'down', 'non-mover']; // BACKUP - DO NOT DELETE
+        }
+        
+        xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", "http://www.bbc.co.uk/radio1/chart/" + type, false );
+        xmlHttp.send( null );
+        data = xmlHttp.responseText;
+
+        function getTitles() {
+            var titles = [];
+            var temp = data.split('<div class="cht-entry-title">');
+            for (var i = 1; i < temp.length; i++) {
+                var temp1 = temp[i].split("</div>")[0].trim();
+                titles.push(temp1);
+            }
+            return titles;
+        }
+
+        function getTitle(position) {
+            var temp = data.split('<div class="cht-entry-title">');
+            if (position > 0 && position < 41) {
+                var title = temp[position].split("</div>")[0].trim();
+            } else {
+                var title = "'position' parameter invalid [getTitle(position)]. Please see documentation for more info.";
+                if (top40_config[0] == 'true') {
+                    console.log(title);
+                }
+            }
+            return title;
+        }
+
+        function getArtists() {
+            var artists = [];
+            var temp = data.split('<div class="cht-entry-artist">');
+            for (var i = 1; i < temp.length; i++) {
+                var temp1 = temp[i].split("</a>")[0].trim().split('">')[1].trim();
+                artists.push(temp1);
+            }
+            return artists;
+        }
+
+        function getArtist(position) {
+            var temp = data.split('<div class="cht-entry-artist">');
+            if (position > 0 && position < 41) {
+                var artist = temp[position].split("</a>")[0].trim().split('">')[1].trim();
+            } else {
+                var artist = "'position' parameter invalid [getArtist(position)]. Please see documentation for more info.";
+                if (top40_config[0] == 'true') {
+                    console.log(artist);
+                }
+            }
+            return artist;
+        }
+
+        function getWeeksInChart() {
+            var weeksInChart = [];
+            var temp = data.split('<div class="cht-entry-status">');
+            for (var i = 1; i < temp.length; i++) {
+                var temp1 = temp[i].split("</div>")[0].trim().toUpperCase().split("|")[1].split("W")[0].trim();
+                weeksInChart.push(temp1);
+
+            }
+            return weeksInChart;
+        }
+
+        function getWeekInChart(position) {
+            var temp = data.split('<div class="cht-entry-status">');
+            if (position > 0 && position < 41) {
+                var weekInChart = temp[position].split("</div>")[0].trim().toUpperCase().split("|")[1].split("W")[0].trim();
+            } else {
+                var weekInChart = "'position' parameter invalid [getWeekInChart(position)]. Please see documentation for more info.";
+                if (top40_config[0] == 'true') {
+                    console.log(weekInChart);
+                }
+            }
+            return weekInChart;
+        }
+
+        function getStates() {
+            var states = [];
+            var temp = data.split('<div class="cht-entry-status">');
+            for (var i = 1; i < temp.length; i++) {
+                if (top40_config[1] == 'lower') {
+                    var temp1 = temp[i].split("</div>")[0].trim().toLowerCase().split("|")[0].trim();
+                } else {
+                    var temp1 = temp[i].split("</div>")[0].trim().toUpperCase().split("|")[0].trim();
+                }
+                states.push(temp1);
+
+            }
+            return states;
+        }
+
+        function getState(position) {
+            var temp = data.split('<div class="cht-entry-status">');
+            if (position > 0 && position < 41) {
+                if (top40_config[1] == 'lower') {
+                    var state = temp[position].split("</div>")[0].trim().toLowerCase().split("|")[0].trim();
+                } else {
+                    var state = temp[position].split("</div>")[0].trim().toUpperCase().split("|")[0].trim();
+                }
+            } else {
+                var state = "'position' parameter invalid [getState(position)]. Please see documentation for more info.";
+                if (top40_config[0] == 'true') {
+                    console.log(state);
+                }
+            }
+            return state;
+        }
+
+        function getAlbumCovers(response) {
+            var albumCovers = [];
+            var temp = data.split('<img class="cht-entry-image"');
+            for (var i = 1; i < temp.length; i++) {
+                var temp1 = temp[i].split('alt="')[0].trim().split('src="')[1].split('"')[0].trim();
+                albumCovers.push(temp1);
+            }
+            return albumCovers;
+        }
+
+        function getAlbumCover(position) {
+            var temp = data.split('<img class="cht-entry-image"');
+            if (position > 0 && position < 41) {
+                var albumCover = temp[position].split('alt="')[0].trim().split('src="')[1].split('"')[0].trim();
+            } else {
+                var albumCover = "'position' parameter invalid [getAlbumCover(position)]. Please see documentation for more info.";
+                if (top40_config[0] == 'true') {
+                    console.log(albumCover);
+                }
+            }
+            return albumCover;
+        }
+
+        function getMovements() {
+            var movementImgs = [];
+            var temp = data.split('<img class="cht-entry-movement-image"');
+            for (var i = 1; i < temp.length; i++) {
+                if (temp[i].split('" />')[0].trim().split('src="')[1].trim() == "http://static.bbci.co.uk/radio/66/1.68/img/icons/chart_nonmover.png") {
+                    var temp1 = top40_config[5];
+                } else if (temp[i].split('" />')[0].trim().split('src="')[1].trim() == "http://static.bbci.co.uk/radio/66/1.68/img/icons/chart_down.png") {
+                    var temp1 = top40_config[4];
+                } else if (temp[i].split('" />')[0].trim().split('src="')[1].trim() == "http://static.bbci.co.uk/radio/66/1.68/img/icons/chart_up.png") {
+                    var temp1 = top40_config[3];
+                } else {
+                    var temp1 = top40_config[2];
+                }
+                movementImgs.push(temp1);
+            }
+            return movementImgs;
+        }
+
+        function getMovement(position) {
+            var temp = data.split('<img class="cht-entry-movement-image"');
+            if (position > 0 && position < 41) {
+                if (temp[position].split('" />')[0].trim().split('src="')[1].trim() == "http://static.bbci.co.uk/radio/66/1.68/img/icons/chart_nonmover.png") {
+                    var movement = top40_config[5];
+                } else if (temp[position].split('" />')[0].trim().split('src="')[1].trim() == "http://static.bbci.co.uk/radio/66/1.68/img/icons/chart_down.png") {
+                    var movement = top40_config[4];
+                } else if (temp[position].split('" />')[0].trim().split('src="')[1].trim() == "http://static.bbci.co.uk/radio/66/1.68/img/icons/chart_up.png") {
+                    var movement = top40_config[3];
+                } else {
+                    var movement = top40_config[2];
+                }
+            } else {
+                var movement = "'position' parameter invalid [getAlbumCover(position)]. Please see documentation for more info.";
+                if (top40_config[0] == 'true') {
+                    console.log(movement);
+                }
+            }
+            return movement;
+        }
+    } else {
+        this.getTitles = alertError,
+        this.getTitle = alertError,
+        this.getArtists = alertError,
+        this.getArtist = alertError,
+        this.getWeeksInChart = alertError,
+        this.getWeekInChart = alertError,
+        this.getStates = alertError,
+        this.getState = alertError,
+        this.getAlbumCovers = alertError,
+        this.getAlbumCover = alertError,
+        this.getMovements = alertError,
+        this.getMovement = alertError;
+        if (top40_config[0] == 'true') {
+            console.log("'type' parameter is invalid. Please see documentation for valid parameters.");
+        }
+        console.log("'type' parameter is invalid. Please see documentation for valid parameters.");
+        
+        function alertError() {
+            if (top40_config[0] == 'true') {
+                console.log("'type' parameter is invalid. Please see documentation for valid parameters.");
+            }
+        }
+    }
+}
